82b15ccd95304adcb0482349b15b4613603dfec3
Yash Kapoor <kapooryash713@gmail.com>
2021-07-25 22:21:15 +0530

    Dice combination iterative

---
+++ Dice Combinatons.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<long long> dp(n+1,0);
+    dp[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=6;j++)
+        {
+            if(i-j>=0)
+            {
+                dp[i]+=dp[i-j];
+                dp[i]=dp[i]%1000000007;
+            }
+        }
+    }
+    
+    cout<<dp[n];
+    }
+
